7bd7633009e7ce98e8e414a29d619da007342278
zhangqian <wb_zhangqian13@meituan.com>
2025-12-09 11:56:35 +0800

    feat: 添加自定义底部导航栏组件并集成到首页和个人页
    -新增custom-tab-bar组件，实现底部导航栏功能；更新首页和个人页配置及布局，支持导航栏状态同步显示当前选中页。

--- miniprogram/components/custom-tab-bar/index.ts
+++ miniprogram/components/custom-tab-bar/index.ts
@@ -4,27 +4,40 @@ Component({
     color: "#7A7E83",
     selectedColor: "#3cc51f",
     list: [{
-      pagePath: "/index/index",
+      pagePath: "/pages/index/index",
       iconPath: "/image/home.png",
       selectedIconPath: "/image/home_active.png",
       text: "首页"
     }, {
-      pagePath: "/index/index2",
+      pagePath: "/pages/person/index",
       iconPath: "/image/person.png",
       selectedIconPath: "/image/person_active.png",
       text: "我的"
     }]
   },
   attached() {
+    this.updateSelectedTab()
   },
   methods: {
+    updateSelectedTab() {
+      const pages = getCurrentPages()
+      const currentPage = pages[pages.length - 1]
+      const currentPath = currentPage.route
+
+      const list = this.data.list
+      for (let i = 0; i < list.length; i++) {
+        if (currentPath === list[i].pagePath.replace(/^\/pages\//, '')) {
+          this.setData({
+            selected: i
+          })
+          break
+        }
+      }
+    },
     switchTab(e: any) {
       const data = e.currentTarget.dataset
       const url = data.path
       wx.switchTab({url})
-      this.setData({
-        selected: data.index
-      })
     }
   }
 })
